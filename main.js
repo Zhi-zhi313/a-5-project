a26353c52d9b51d913ba9200eac244f2a31e8f46
Hughchen2046 <hughchen2046@gmail.com>
2025-09-27 15:09:39 +0800

    完成首頁資訊橫幅

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 // 新增swiper
-var swiper = new Swiper(".headerSwiper", {
+var headerswiper = new Swiper(".headerSwiper", {
       spaceBetween: 30,
       effect: "fade",
+      loop: true,
+      lazy: true,
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -17,3 +19,15 @@ var swiper = new Swiper(".headerSwiper", {
         },
     },
 });
+
+    var indexswiper = new Swiper(".indexSwiper", {
+      slidesPerView: "auto",
+      lazy: true,
+      freeMode: true,
+      spaceBetween: 12,
+      breakpoints: {
+        768: {
+          spaceBetween: 24,
+        },
+      },
+    });
